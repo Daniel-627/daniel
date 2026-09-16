efebd9f06e8e7557c464c990237bcd5c1247ebe5
Daniel Ochieng <ochiengdaniel627@gmail.com>
2026-09-16 21:47:44 +0300

    Fix schema export name in schemaTypes/index.ts

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,7 +1,11 @@
+import { type SchemaTypeDefinition } from "sanity";
+
 import project from "./project";
 import testimonial from "./testimonial";
 import clientLogo from "./clientLogo";
 import service from "./service";
 import processStep from "./processStep";
 
-export const schemaTypes = [project, testimonial, clientLogo, service, processStep];
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [project, testimonial, clientLogo, service, processStep],
+};
